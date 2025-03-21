899811d891f67cff5cee183fa78467c387e447be
Shadab-2604 <138616478+Shadab-2604@users.noreply.github.com>
2025-03-21 23:02:03 +0530

    port 3000

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(express.json());
 
 app.use("/", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
